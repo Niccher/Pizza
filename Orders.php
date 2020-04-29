2d462cd1a86b346a8e6a0671366fa8fd1ae36bac
Nicch <domi777nicch@gmail.com>
2020-04-29 19:04:23 +0300

    Added the Pizza listing

--- Orders.php
+++ Orders.php
@@ -24,7 +24,7 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="index2.php">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item active">
             <a class="nav-link" href="Orders.php">Orders</a>
